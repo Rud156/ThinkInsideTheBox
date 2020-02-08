21766972a074d660b7e5f9478540b76aa7655a05
Rud156 <rudrangshu_1997@yahoo.com>
2020-02-08 00:20:23 -0700

    Added Player Follower

---
+++ Assets/Scripts/WorldCube/CubeLayerPlayerFollower.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace WorldCube
+{
+    public class CubeLayerPlayerFollower : MonoBehaviour
+    {
+        public Transform playerTransform;
+        public Vector3 positionOffset;
+
+        #region Unity Functions
+
+        private void Update()
+        {
+            Vector3 targetPosition = playerTransform.position;
+            targetPosition += positionOffset;
+
+            transform.position = targetPosition;
+        }
+
+        #endregion
+    }
+}
