6adfb6a45fcd339a0614d88d100f8f3a34ddd181
Rud156 <rudrangshu_1997@yahoo.com>
2020-02-08 21:52:30 -0700

    Added Grid Player Follower

---
+++ Assets/Scripts/Player/PlayerGridPositionMarker.cs
@@ -0,0 +1,42 @@
+﻿using Extensions;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerGridPositionMarker : MonoBehaviour
+    {
+        [Header("Transforms")] public Vector3 gridStartTransform;
+        public Transform playerTransform;
+
+        [Header("Position Info")] public int sideSize;
+
+        private Vector3 m_currentGridCenter;
+
+        #region Unity Functions
+
+        private void Update()
+        {
+            Vector3 playerPosition = playerTransform.position;
+
+            float xValue = ExtensionFunctions.GetClosestMultiple(playerPosition.x);
+            float yValue = ExtensionFunctions.GetClosestMultiple(playerPosition.y);
+            float zValue = ExtensionFunctions.GetClosestMultiple(playerPosition.z);
+
+            m_currentGridCenter.Set(xValue, yValue, zValue);
+            m_currentGridCenter -= gridStartTransform;
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                Debug.Log(m_currentGridCenter);
+            }
+        }
+
+        #endregion
+
+        #region External Functions
+
+        public Vector3 GridPosition => m_currentGridCenter;
+
+        #endregion
+    }
+}
--- Assets/Scripts/WorldCube/CubeLayerPlayerFollower.cs
+++ Assets/Scripts/WorldCube/CubeLayerPlayerFollower.cs
@@ -1,20 +1,18 @@
-﻿using UnityEngine;
+﻿using Player;
+using UnityEngine;
 
 namespace WorldCube
 {
     public class CubeLayerPlayerFollower : MonoBehaviour
     {
-        public Transform playerTransform;
-        public Vector3 positionOffset;
+        public PlayerGridPositionMarker playerGridPositionMarker;
 
         #region Unity Functions
 
         private void Update()
         {
-            Vector3 targetPosition = playerTransform.position;
-            targetPosition += positionOffset;
-
-            transform.position = targetPosition;
+            Vector3 followPosition = playerGridPositionMarker.GridPosition;
+            transform.position = followPosition;
         }
 
         #endregion
