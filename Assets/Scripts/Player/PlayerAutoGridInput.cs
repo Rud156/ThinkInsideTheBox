ec80eb66e543dfba2c83584921ed5273c14d5c23
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2020-02-13 16:48:20 -0700

    Base Script

---
+++ Assets/Scripts/Player/PlayerAutoGridInput.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class PlayerAutoGridInput : MonoBehaviour
+    {
+        public float stopTimeBetweenPositions;
+        public Direction startDirection;
+
+        private float m_currentTimer;
+        private bool m_isMovementActive;
+
+        private Direction m_currentDirection;
+
+        #region Unity Functions
+
+        private void Start()
+        {
+            m_currentDirection = startDirection;
+        }
+
+        private void Update()
+        {
+        }
+
+        #endregion
+
+        #region External Functions
+
+        public void StartPlayerMovement()
+        {
+            m_isMovementActive = true;
+        }
+
+        public void StopPlayerMovement()
+        {
+            m_isMovementActive = false;
+        }
+
+        #endregion
+
+        #region Utility Functions
+
+        private void UpdateMovementTimer()
+        {
+            if (!m_isMovementActive)
+            {
+                return;
+            }
+
+            m_currentTimer -= Time.deltaTime;
+            if (m_currentTimer <= 0)
+            {
+                FindNextMovementSpot();
+
+                m_currentTimer = stopTimeBetweenPositions;
+            }
+        }
+
+        private void FindNextMovementSpot()
+        {
+        }
+
+        #endregion
+
+        #region Enums
+
+        public enum Direction
+        {
+            Forward,
+            Backward,
+            Left,
+            Right
+        }
+
+        #endregion
+    }
+}
