ec46aacf03dc95e433596b8e1636f0fbcca8896d
Trevor Alex <pascerveau@gmail.com>
2020-02-14 09:31:28 -0700

    Change InventorySystem to Singleton, change destination door collision type to collider

--- Assets/Scripts/Inventory/Collect.cs
+++ Assets/Scripts/Inventory/Collect.cs
@@ -5,27 +5,15 @@ using Utils;
 
 public class Collect : MonoBehaviour
 {
-    public InventorySystem myInventory;
 
     #region Unity Functions
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider i_other)
     {
 
         if (i_other.gameObject.CompareTag(TagManager.Player))
         {
-            myInventory.MarkCollected(this.gameObject);
+            InventorySystem.Instance.MarkCollected(this.gameObject);
             Destroy(this.gameObject);
         }
     }
