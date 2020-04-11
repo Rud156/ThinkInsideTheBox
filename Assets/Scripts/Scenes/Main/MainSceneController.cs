e234f8678c41ac22096f0e066ba31cfdda0cba26
Rud156 <rudrangshu_1997@yahoo.com>
2020-04-10 22:34:22 -0600

    Updated With Custom Movement for Special Cases

--- Assets/Scripts/Scenes/Main/MainSceneController.cs
+++ Assets/Scripts/Scenes/Main/MainSceneController.cs
@@ -19,6 +19,18 @@ namespace Scenes.Main
         public int worldNumber = -1;
         public float sceneTransitionEndDelay = 1f;
 
+        [Header("Custom Player Movement")] public bool useCustomPlayerMovement;
+        public DummyTimeLineAnimation playerAnimation;
+        public float playerLerpSpeed;
+        public List<Transform> playerMovementPositions;
+        public CubeLayerMask finalMovementDirection;
+
+        private Vector3 m_startPosition;
+        private Vector3 m_targetPosition;
+        private float m_lerpAmount;
+        private int m_currentPositionIndex;
+        private bool m_isLerpActive;
+
         private Transform m_playerTransform;
 
         #region Unity Functions
@@ -32,6 +44,43 @@ namespace Scenes.Main
         }
 
         private void OnDestroy() => FaceObject.OnLoaded -= UpdateSceneDataOnSwitch;
+
+        private void Update()
+        {
+            if (!m_isLerpActive)
+            {
+                return;
+            }
+
+            m_lerpAmount += playerLerpSpeed * Time.deltaTime;
+            if (m_lerpAmount >= 0.97f)
+            {
+                m_playerTransform.position = m_targetPosition;
+                Debug.Log("Lerp Ended. Checking and Switching");
+
+                playerAnimation.PlayIdleAnim(); // Reset Anim State
+                m_lerpAmount = 0;
+                m_currentPositionIndex += 1;
+
+                if (m_currentPositionIndex >= playerMovementPositions.Count)
+                {
+                    m_isLerpActive = false;
+                    playerController.ManuallyMoveTo(finalMovementDirection);
+                    playerManualMovement.EnableMovement();
+                    Debug.Log("Player Movement Complete");
+                }
+                else
+                {
+                    m_startPosition = m_playerTransform.position;
+                    m_targetPosition = playerMovementPositions[m_currentPositionIndex].position;
+                    playerAnimation.PlayJumpAnim();
+                }
+            }
+            else
+            {
+                m_playerTransform.position = Vector3.Lerp(m_startPosition, m_targetPosition, m_lerpAmount);
+            }
+        }
 
         #endregion
 
@@ -51,8 +100,23 @@ namespace Scenes.Main
 
             yield return new WaitForSeconds(sceneTransitionEndDelay);
 
-            playerController.ManuallyMoveTo(playerSpawn.initialPlayerMovementDirection);
-            playerManualMovement.EnableMovement();
+            if (useCustomPlayerMovement)
+            {
+                m_currentPositionIndex = 0;
+
+                m_startPosition = m_playerTransform.position;
+                m_targetPosition = playerMovementPositions[m_currentPositionIndex].position;
+                m_lerpAmount = 0;
+                m_isLerpActive = true;
+                playerAnimation.PlayJumpAnim();
+
+                Debug.Log("Started Custom Player Movement");
+            }
+            else
+            {
+                playerController.ManuallyMoveTo(playerSpawn.initialPlayerMovementDirection);
+                playerManualMovement.EnableMovement();
+            }
         }
 
         private void UpdateSceneDataOnSwitch() => SceneData.LastWorldNumber = worldNumber;
